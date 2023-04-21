be2aa5480f5f2cbe3e9b3970ca1d56b0fad27010
Binh Tran <tranhuubinh110@gmail.com>
2023-04-21 11:24:26 +0700

    update code

--- pages/dichvutrongoi/[id].vue
+++ pages/dichvutrongoi/[id].vue
@@ -64,7 +64,12 @@
                 <b-icon><Dot /></b-icon>Ẩm thực
               </p>
             </div>
-            <b-button small class="w-full mt-4">ĐẶT GÓI</b-button>
+            <b-button
+              small
+              class="w-full mt-4"
+              @click="handleDatGoi(' Gói Minimum')"
+              >ĐẶT GÓI</b-button
+            >
           </b-card>
         </b-col>
         <b-col span="6" :md="3" class="h-auto">
@@ -93,7 +98,12 @@
                 <b-icon><Dot /></b-icon>Ẩm thực
               </p>
             </div>
-            <b-button small class="w-full mt-4">ĐẶT GÓI</b-button>
+            <b-button
+              small
+              class="w-full mt-4"
+              @click="handleDatGoi('Gói Gold')"
+              >ĐẶT GÓI</b-button
+            >
           </b-card>
         </b-col>
         <b-col span="6" :md="3" class="h-auto">
@@ -125,7 +135,12 @@
                 <b-icon><Dot /></b-icon>Ẩm thực
               </p>
             </div>
-            <b-button small class="w-full mt-4">ĐẶT GÓI</b-button>
+            <b-button
+              small
+              class="w-full mt-4"
+              @click="handleDatGoi('Gói Luxury')"
+              >ĐẶT GÓI</b-button
+            >
           </b-card>
         </b-col>
         <b-col span="6" :md="3" class="h-auto">
@@ -164,23 +179,131 @@
                 <b-icon><Dot /></b-icon>Ẩm thực
               </p>
             </div>
-            <b-button small class="w-full mt-4">ĐẶT GÓI</b-button>
+            <b-button small class="w-full mt-4" @click="handleDatGoi('Gói Vip')"
+              >ĐẶT GÓI</b-button
+            >
           </b-card>
         </b-col>
       </b-row>
     </div>
   </b-main>
+
+  <b-dialog v-model="state.dialogVisible" title="Thông Tin Của Bạn">
+    <b-form @submit.prevent="handleSubmit">
+      <b-row>
+        <b-col span="6">
+          <b-form-item label="Họ tên">
+            <b-input
+              v-model.trim="state.infoBook.name"
+              placeholder="Họ tên"
+              clearable
+            />
+          </b-form-item>
+        </b-col>
+        <b-col span="6">
+          <b-form-item label="Số điện thoại">
+            <b-input
+              v-model.trim="state.infoBook.phone"
+              placeholder="Nhập số điện thoại của bạn"
+              clearable
+            />
+          </b-form-item>
+        </b-col>
+        <b-col span="6">
+          <b-form-item label="Ngày book dịch vụ">
+            <b-date-picker
+              v-model.trim="state.infoBook.date"
+              type="date"
+              placeholder="Ngày book dịch vụ"
+            />
+          </b-form-item>
+        </b-col>
+        <b-col span="6">
+          <b-form-item label="Địa chỉ của bạn">
+            <b-input
+              v-model.trim="state.infoBook.address"
+              placeholder="Nhập địa chỉ của bạn"
+              clearable
+            />
+          </b-form-item>
+        </b-col>
+        <b-col span="6">
+          <b-form-item label="Gói dịch vụ">
+            <b-input
+              v-model.trim="state.infoBook.package"
+              placeholder="Gói dịch vụ"
+              clearable
+            />
+          </b-form-item>
+        </b-col>
+      </b-row>
+    </b-form>
+    <template #footer>
+      <div class="flex justify-end">
+        <b-button class="mr-4" @click="state.dialogVisible = false"
+          >Quay Lại</b-button
+        >
+        <b-button type="primary" @click="handleSubmit">Hoàn Tất</b-button>
+      </div>
+    </template>
+  </b-dialog>
+
+  <b-dialog
+    xsmall
+    center
+    v-model="state.dialogSuccess"
+    :show-close="false"
+    header-class="p-0"
+  >
+    <div class="text-center">
+      <b-lottie
+        url="/lottiefiles/succes.json"
+        :width="200"
+        :height="200"
+        class="m-auto"
+      />
+      <p class="text-2xl mb-6">Bạn đã lên lịch thành công!</p>
+      <b-button class="w-full" @click="router.push('/')">Trang chủ</b-button>
+      <b-button class="w-full" @click="handleSucces">Xem lịch</b-button>
+    </div>
+  </b-dialog>
 </template>
 
 <script setup lang="ts">
 import { Dot } from "@bigin/icons-vue";
 const route = useRoute();
+const router = useRouter();
 
 const state = reactive({
   tenDichVu: "",
+  dialogVisible: false,
+  dialogSuccess: false,
+  infoBook: {
+    name: "",
+    phone: "",
+    date: "",
+    address: "",
+    package: "",
+  },
 });
 
 state.tenDichVu = route.params.id as string;
+
+const handleSubmit = () => {
+  state.dialogVisible = false;
+  localStorage.setItem("booking", JSON.stringify(state.infoBook));
+  state.dialogSuccess = true;
+};
+
+const handleDatGoi = (val: string) => {
+  state.dialogVisible = true;
+  state.infoBook.package = val;
+};
+
+const handleSucces = () => {
+  state.dialogSuccess = false;
+  router.push("/lichdat");
+};
 </script>
 
 <style scoped></style>
---
+++ pages/lichdat/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>lịch đặt</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
